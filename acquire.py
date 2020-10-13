8c6c8444da77243c629ddd454f9f6dd4eead9825
Austin Aranda <austinaranda10@gmail.com>
2020-10-13 15:48:21 -0500

    update

--- acquire.py
+++ acquire.py
@@ -15,20 +15,35 @@ def get_connection(db, user=user, host=host, password=password):
 
 def get_zillow_data():
     
-    sql_query = '''SELECT p17.*, pt.propertylandusetypeid, pred17.logerror, pred17.transactiondate
-                FROM properties_2017 AS p17
-                JOIN propertylandusetype as pt ON p17.propertylandusetypeid = pt.propertylandusetypeid
-                JOIN predictions_2017 AS pred17 ON p17.parcelid = pred17.parcelid
-                WHERE pred17.transactiondate BETWEEN '2017-05-01' AND '2017-06-30'
-                AND pt.propertylandusetypeid <> 31 AND pt.propertylandusetypeid <> 46 
-                AND pt.propertylandusetypeid <> 47 AND pt.propertylandusetypeid <> 267 
-                AND pt.propertylandusetypeid <> 269 AND pt.propertylandusetypeid <> 270 
-                AND pt.propertylandusetypeid <> 271 AND pt.propertylandusetypeid <> 274 
-                AND pt.propertylandusetypeid <> 273 AND pt.propertylandusetypeid <> 290 
-                AND pt.propertylandusetypeid <> 291'''
+    sql_query = '''
+                select * from properties_2017
+                join (select id, logerror, pid, tdate from predictions_2017 pred_2017
+                join (SELECT parcelid as pid, Max(transactiondate) as tdate FROM predictions_2017 GROUP BY parcelid) as sq1
+                on (pred_2017.parcelid = sq1.pid and pred_2017.transactiondate = sq1.tdate)) as sq2
+                on (properties_2017.parcelid = sq2.pid)
+                left join airconditioningtype using (airconditioningtypeid)
+                left join architecturalstyletype using (architecturalstyletypeid)
+                left join buildingclasstype using (buildingclasstypeid)
+                left join heatingorsystemtype using (heatingorsystemtypeid)
+                left join propertylandusetype using (propertylandusetypeid)
+                left join storytype using (storytypeid)
+                left join typeconstructiontype using (typeconstructiontypeid)
+                left join unique_properties using (parcelid)
+                where latitude is not null and longitude is not null;
+'''
     
     df = pd.read_sql(sql_query, get_connection('zillow'))
-    df.to_csv('zillow_hot_months.csv')
+    df.to_csv('zillow.csv')
     return df
 
 print('acquire.py functions loaded successfully')
+
+
+############################################Summary of Missing Values#################################
+
+def missing_data(df):
+    num_rows_missing = df.isnull().sum()
+    pct_rows_missing = df.isnull().sum() / (df.isnull().sum() + df.notnull().sum())
+    nulls = pd.DataFrame({'num_rows_missing':num_rows_missing,'pct_rows_missing':pct_rows_missing})
+    return nulls
+
